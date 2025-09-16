e89bc78114714e8bf6911f97a00f48bb4d322b74
Luiz Miguel <luiztw.tv@gmail.com>
2025-09-16 15:13:31 -0300

    finalização do codigo

--- aula_1/main.py
+++ aula_1/main.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
 
-pyautogui.PAUSE = 0.6
+pyautogui.PAUSE = 0.3
 
 #Passo 1: Entrar no sistema da empresa
 #abrir o google
@@ -26,6 +26,90 @@ pyautogui.press('tab')
 pyautogui.press('enter')
 
 #Passo 3: Importar os dados
+import pandas
+
+tabela = pandas.read_csv('produtos.csv')
+print(tabela)
 
 #Passo 4: Cadastrar 1ª produto
+#    pyautogui.click(x=678, y=290)
+#
+#    codigo = 'MOLO000251'
+#    pyautogui.write(codigo)
+#    time.sleep(0.3)
+#    pyautogui.press('tab')
+#
+#    marca = 'Logitech'
+#    pyautogui.write(marca)
+#   time.sleep(0.3)
+#    pyautogui.press('tab')
+#
+#
+#    tipo = 'Mouse'
+#    pyautogui.write(tipo)
+#   time.sleep(0.3)
+#    pyautogui.press('tab')
+#
+#
+#    categoria = '1'
+#    pyautogui.write(categoria)
+#    time.sleep(0.3)
+#    pyautogui.press('tab')
+#
+#
+#    preco_unitario = '25.95'
+#    pyautogui.write(preco_unitario)
+#    time.sleep(0.3)
+#    pyautogui.press('tab')
+#
+#
+#    custo = '6.50'
+#    pyautogui.write(custo)
+#    time.sleep(0.3)
+#    pyautogui.press('tab')
+#
+#
+#    obs = ''
+#    pyautogui.write(obs)
+#    time.sleep(0.3)
+#
+#    pyautogui.press('tab')
+#    pyautogui.press('enter')
+#    pyautogui.scroll(10000)
+#
 #Passo 5: Repetir para os proximos
+for linha in tabela.index:
+    pyautogui.click(x=678, y=290)
+
+    codigo = tabela.loc[linha, 'codigo']
+    pyautogui.write(codigo)
+    pyautogui.press('tab')
+
+    marca = tabela.loc[linha, 'marca']
+    pyautogui.write(marca)
+    pyautogui.press('tab')
+
+    tipo = tabela.loc[linha, 'tipo']
+    pyautogui.write(tipo)
+    pyautogui.press('tab')
+
+    categoria = str(tabela.loc[linha, 'categoria'])
+    pyautogui.write(categoria)
+    pyautogui.press('tab')
+
+    preco_unitario = str(tabela.loc[linha, 'preco_unitario'])
+    pyautogui.write(preco_unitario)
+    pyautogui.press('tab')
+
+    custo = str(tabela.loc[linha, 'custo'])
+    pyautogui.write(custo)
+    pyautogui.press('tab')
+
+    obs = str(tabela.loc[linha, 'obs'])
+    pyautogui.write(obs)
+    time.sleep(0.1)
+
+
+
+    pyautogui.press('enter')
+    pyautogui.scroll(10000)
